0d0613209e633d8f723cb35fd4adbfc47324281b
Arshia01 <70737240+Arshia01@users.noreply.github.com>
2020-12-08 23:33:28 +0530

    adding milestone (#46)

---
+++ Arshia/Milestone-14/Flipkart/Javascript/app.js
@@ -0,0 +1,31 @@
+ //product slider
+/*
+ const prev = document.querySelector('.prev');
+ const next = document.querySelector('.next');
+ const allProducts = document.querySelectorAll('.product');
+ const focus = document.querySelector('.focus');
+ const size = allProducts[0].clientWidth;
+
+ const firstItem = document.getElementById('first-item');
+ let counter=0;
+
+ function pressedPrev(e){
+    if(counter <= 0){
+        return;
+    }
+    focus.style.transition = `transform 500ms ease-in-out`;
+    counter--;
+    focus.style.transform = `translateX(${-size * counter}px)`;
+ }
+
+ function pressedNext(){
+     focus.style.transition = `transform 500ms ease-in-out`;
+     counter++;
+     focus.style.transform = 'translateX(' + (-size * counter) + 'px)';
+     console.log(counter);
+ }
+
+ prev.addEventListener('click',pressedPrev);
+ next.addEventListener('click',pressedNext);*/
+
+ 
