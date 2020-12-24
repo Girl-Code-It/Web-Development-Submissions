cb9cdff636615dd607a91384b252dabe25340528
Vishal Rajput <59874304+Vishal-raj-1@users.noreply.github.com>
2020-12-24 13:39:27 +0530

    Milestone 14 Done. (#69)
    * Updated
    
    Remove /*Done*/
    
    * Milestone 11 is done
    
    * Milestone 12 is done
    
    * JS projects are done
    
    * Adding Flipkart Homepage Clone

---
+++ Vishal/Milestone 14 Web Clone Projects 🔥/Flipkart/script.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('.Header');
+const nav = document.querySelector('.Navbar')
+const topOfNav = header.offsetTop;
+
+function fixNav(){
+    if (window.scrollY >= topOfNav){
+        nav.style.paddingTop = header.offsetHeight + 2 + 'px';
+        header.classList.add('.fixed-top');
+    }
+    else{
+        nav.style.paddingTop = 0;
+    }
+}
+// for fixing the navbar at top
+window.addEventListener('scroll',fixNav);
